01cc0f5141342532d601eb96ed0262484a64ce76
Paulo Vitor Francisco <paulovfrancisco@gmail.com>
2026-07-02 13:19:33 -0300

    feat: add libs and create design system (#5)
    * add libs and create design system
    
    * remove comments

---
+++ tailwind.config.ts
@@ -0,0 +1,39 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          50: '#f0f7ff',
+          100: '#e0effe',
+          500: '#0ea5e9',
+          600: '#0284c7',
+          700: '#0369a1',
+          900: '#0c4a6e',
+        },
+        success: '#10b981',
+        warning: '#f59e0b',
+        danger: '#ef4444',
+        
+        neutral: {
+          50: '#f8fafc', 
+          100: '#f1f5f9',
+          200: '#e2e8f0',
+          800: '#1e293b',
+          900: '#0f172a',
+        }
+      },
+      fontFamily: {
+        sans: ['var(--font-sans)', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
